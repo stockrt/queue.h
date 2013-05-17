7232e07b1cfe021051272ea964057dfe24749185
Rogério Carvalho Schneider <stockrt@gmail.com>
2013-05-17 00:59:32 -0300

    Shannon random number generator

---
+++ shannon.c
@@ -0,0 +1,24 @@
+#include <limits.h>
+
+long int I_SEED = 123456789;
+
+// Shannon random number generator.
+// Random number between 0.0 and 1.0.
+double randshannon(void)
+{
+    double random=0;
+    long int i=0, j=0;
+
+    I_SEED = 16807 * I_SEED;
+
+    if (I_SEED < 0) {
+        I_SEED += LONG_MAX;
+    }
+
+    i = I_SEED / 32767;
+    j = I_SEED - i * 32767;
+
+    random = j / 32767.;
+
+    return random;
+}
